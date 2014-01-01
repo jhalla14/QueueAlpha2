95a84a6c2823b2d90edcee7522e3c76dd1bee389
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2014-01-01 17:35:26 -0600

    created start up VC

---
+++ QueueAlpha2/QueueAlpha2/StartUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  StartUpViewController.h
+//  QueueAlpha2
+//
+//  Created by Joshua Hall  on 1/1/14.
+//  Copyright (c) 2014 Queue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartUpViewController : UIViewController
+
+@end
