676ca0d5087d8ebda17590fc568d8e8f45a4382b
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2014-01-01 17:29:34 -0600

    inital upload to repo

---
+++ QueueAlpha2/QueueAlpha2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QueueAlpha2
+//
+//  Created by Joshua Hall  on 1/1/14.
+//  Copyright (c) 2014 Queue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QueueAlpha2/QueueAlpha2Tests/QueueAlpha2Tests.m
@@ -0,0 +1,34 @@
+//
+//  QueueAlpha2Tests.m
+//  QueueAlpha2Tests
+//
+//  Created by Joshua Hall  on 1/1/14.
+//  Copyright (c) 2014 Queue. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QueueAlpha2Tests : XCTestCase
+
+@end
+
+@implementation QueueAlpha2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
